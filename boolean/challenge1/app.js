aec0a18d93b2086c1e52f6c7165a83b453d5dd38
hasbi-alaziz <94413730+hasbi-alajiz@users.noreply.github.com>
2025-11-15 11:03:54 +0700

    penyelesaian soal

---
+++ boolean/challenge1/app.js
@@ -0,0 +1,11 @@
+const hasChild = false;
+const isStudents = false;
+const isEmployeed = true;
+const isRide = true;
+const hasPet = true;
+
+console.log(hasChild);
+console.log(isStudents);
+console.log(isEmployeed);
+console.log(isRide);
+console.log(hasPet);
